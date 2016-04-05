7495c349b5075e8e1046af5f6bc756b7a6d1464e
taku00jp <saitou@saitou-no-imac.local>
2016-04-05 18:56:07 +0900

    Initial Commit

---
+++ taskapp1/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  taskapp1
+//
+//  Created by 斉藤 on 2016/04/05.
+//  Copyright © 2016年 taku.saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // MARK: UITableViewDataSourceプロトコルのメソッド
+    // データの数（＝セルの数）を返すメソッド
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // 各セルの内容を返すメソッド
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegateプロトコルのメソッド
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+         performSegueWithIdentifier("cellSegue",sender: nil) // ←追加
+        
+    }
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.Delete
+    }
+    // Delete ボタンが押された時に呼ばれるメソッド
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
+
+
+}
+
---
+++ taskapp1/inputViewController.swift
@@ -0,0 +1,44 @@
+//
+//  inputViewController.swift
+//  taskapp1
+//
+//  Created by 斉藤 on 2016/04/05.
+//  Copyright © 2016年 taku.saito. All rights reserved.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+ 
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
